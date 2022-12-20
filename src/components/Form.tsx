314b4c31e6ea0aee559860334166b9c0468bad5f
Dovlat <dovlat93@gmail.com>
2022-12-20 19:06:12 +0100

    fixes form styles

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -10,9 +10,14 @@ interface FormProps {
 const URL = import.meta.env.VITE_RAPIDAPI_URL;
 
 const Form = ({ fetchData }: FormProps) => {
-  const initialParams = { q: '', dt: '' };
+  const today = new Date().toISOString().split('T')[0];
+  const week = 7 * 24 * 60 * 60 * 1000;
 
+  const lessThanWeek = (then: string, now: string) => Date.parse(today) - Date.parse(then) <= week;
+
+  const initialParams = { q: '', dt: today };
   const [recentSearch, setRecentSearch] = useState<Array<string>>([]);
+  const [errorMsg, setErrorMsg] = useState('');
   const [params, setParams] = useState(initialParams);
   const generateRandomKey = () => Math.random().toString(36).slice(2, 7);
 
@@ -22,7 +27,10 @@ const Form = ({ fetchData }: FormProps) => {
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value, name } = e.target;
-    setParams({ ...params, [name]: value });
+    if ((name === 'dt' && value > today) || lessThanWeek(value, today)) {
+      setParams({ ...params, [name]: value });
+    }
+    setErrorMsg('The date cannot be more than 7 days ago');
   };
 
   const handleSubmit = async (e: React.FormEvent) => {
@@ -34,18 +42,19 @@ const Form = ({ fetchData }: FormProps) => {
       });
     }
     const data = await getWeatherData(URL, params);
+    console.log('🚀 ~ file: Form.tsx:37 ~ handleSubmit ~ data', data);
     fetchData(data as WeatherData);
     setParams(initialParams);
   };
 
   return (
-    <form className="flex items-stretch justify-center gap-4 p-8" onSubmit={handleSubmit}>
-      <div className="flex items-end gap-2">
-        <label htmlFor="search">
+    <form className="flex flex-col items-center justify-center gap-4 p-8" onSubmit={handleSubmit}>
+      <fieldset className="flex w-full flex-col items-center gap-2 px-36">
+        <label htmlFor="search" className="flex w-full flex-col">
           <span>Get data for another city:</span>
           <input
             id="search"
-            className="ml-2 rounded p-2 placeholder:italic"
+            className="rounded p-2 placeholder:italic"
             type="text"
             name="q"
             placeholder="Type a city name..."
@@ -59,11 +68,11 @@ const Form = ({ fetchData }: FormProps) => {
             <option key={generateRandomKey()}>{item}</option>
           ))}
         </datalist>
-        <label htmlFor="date">
-          Choose a date
+        <label htmlFor="date" className="flex w-full flex-col">
+          <span>Choose a date:</span>
           <input
             id="date"
-            className="ml-2 rounded p-2 placeholder:italic"
+            className="rounded p-2 placeholder:italic"
             type="date"
             name="dt"
             placeholder="Type a city name..."
@@ -72,25 +81,26 @@ const Form = ({ fetchData }: FormProps) => {
             onChange={handleChange}
           />
         </label>
-      </div>
+        {errorMsg && <span className="bold italic text-red-400">{errorMsg}</span>}
 
-      <button type="submit" className="flex w-36 items-center justify-center gap-2  rounded bg-sky-500 p-2">
-        <span className="bold text-white">Search</span>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className="h-6 w-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-          />
-        </svg>
-      </button>
+        <button type="submit" className="flex w-full items-center justify-center gap-2  rounded bg-sky-500 p-2">
+          <span className="bold text-white">Search</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="h-6 w-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+            />
+          </svg>
+        </button>
+      </fieldset>
     </form>
   );
 };
